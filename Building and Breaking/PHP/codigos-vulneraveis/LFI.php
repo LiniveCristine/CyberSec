17e6d8434a603e8c1e04fe78e2c81cd953b2ff95
Linive Cristine <linive.cristine2@gmail.com>
2026-03-19 16:30:27 -0300

    Codigo com correção

--- Building and Breaking/PHP/codigos-vulneraveis/LFI.php
+++ Building and Breaking/PHP/codigos-vulneraveis/LFI.php
@@ -1,25 +1,26 @@
 <?php
 
-$files_folder = __DIR__; //-> correção
+$files_folder = __DIR__; 
 
 if(!isset($_GET['filename'])){
     echo "Por favor, informe o filename na query string";
     die();
 }
-    
+
+//$filename = basename($_GET['filename']);// -> correção. 
+
 $filename = $_GET['filename'];
-$filepatch = "{$files_folder}/{$filename}"; //-> correção
+$filepatch = "{$files_folder}/{$filename}"; //-> caminho completo para o arquivo
 
 var_dump($filepatch);
 
-//substituir por filepatch -> correção
+//A busca vai ficar restringinda ao diretório definido em $files_folder
+
 if (!file_exists($filepatch)){ 
     echo "file not found";
     die();
 
 } 
-//A busca vai ficar restringinda ao diretório definido em $files_folder
-
 
 $content = file_get_contents($filepatch);
 echo $content;
